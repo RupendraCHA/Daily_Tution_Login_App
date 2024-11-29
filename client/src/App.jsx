979507e73445588110b764b8a7a1871f8c3dc0c5
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-11-29 23:46:15 +0530

    Project Setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// import all routers
+
+import Username from './components/Username'
+import Password from './components/Password'
+import Register from './components/Register'
+import Recovery from './components/Recovery'
+import Profile from './components/Profile'
+import Reset from './components/Reset'
+import PageNotFound from './components/PageNotFound'
+
+// root routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Root Route</div>
+  },
+  {
+    path: "/register",
+    element: <div>Register Route</div>
+  }
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </main>
   )
 }
 
